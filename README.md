fa61679a3f08f30b806b672d1d6c8f67830ce220
samsatp <sathianpong829@gmail.com>
2021-09-12 20:07:05 +0700

    model inference

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# Topic modelling v2
-This is the NLP project in which I'm trying to achieve the text classification task. The main difference from [Topicmodelling v1](https://github.com/samsatp/topicmodeling) is the *method* and *data* used to train the model which will be discussed in detail in the following section.
+# Topic modelling with zero-shot classification (POC)
+This is the NLP project in which I'm trying to achieve the text classification task. The main difference from [Topicmodelling v1](https://github.com/samsatp/topicmodeling) is the *method* and *data* used to train the model which will be discussed in detail in the following section. This project is not 100% perfect. *The main purpose of it is to prove the concept of zero-shot learning implementation in Tensorflow (POC)*
 
 ## Method
-The model will be building by employ *Zero-shot learning* idea. The model architecture can be illustrated in the figure below.
+The model will be building by employ *Zero-shot learning* idea. The model architecture can be illustrated in the figure below. The data used to train the model can be found at https://www.kaggle.com/Cornell-University/arxiv It's a repository storing research papers in many fields.
+
 ![](./media/model.drawio.png)
+
+## Usage
+- `model.py` contains the model's implementation
+- `inference.py` is the main file used to predict the task
+```bash
+usage: inference.py [-h] [-a ABSTRACT] [-c CANDIDATES]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -a ABSTRACT, --abstract ABSTRACT
+                        A raw query string
+  -c CANDIDATES, --candidates CANDIDATES
+                        Candidates of categories, separates each candidate with '<sep>'
+```
+> NOTE: use `\n` to separate each sentence in document specified in `--abstract` 
+
+Example: `python inference.py --abstract="some document in a long string." --candidates="computer science<sep>economics<sep>geology"`
---
+++ inference.py
@@ -0,0 +1,85 @@
+import tensorflow_hub as hub
+import tensorflow as tf
+from typing import List, Union
+import numpy as np
+import requests
+import argparse
+import json
+import re
+
+model_path = "./saved_model"
+embedding_url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"
+
+model = tf.keras.models.load_model(model_path)
+embedding = hub.KerasLayer(embedding_url, trainable=False)
+
+category_map = json.load(open("./data/mapping.json"))
+tokenizer = json.load(open("./data/cat_tok.json"))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--abstract", help="A raw query string", type=str)
+parser.add_argument("-c", "--candidates", help="Candidates of categories, separates each candidate with '<sep>'", type=str)
+args = parser.parse_args()
+
+
+def splito(text: str):
+    texts = tf.strings.split(text, sep="<br>")
+    return texts
+
+def sentence_tokenize(abstract):
+    return re.sub("\n", "<br>", abstract)  
+
+
+def preprocess_abstract(abstract:str):
+    abstract = abstract.strip()
+    abstract = sentence_tokenize(abstract)
+    return splito(abstract)
+
+
+def preprocess_cadidates(candidates: str):
+    return candidates.split('<sep>')
+
+
+
+
+def get_tokens(candidates: List[str]):
+    all_candidates_tokens = []
+    for cat in candidates:
+        tokens = []
+        cat = cat.split(' ')
+        for word in cat:
+            try:
+                tokens.append(tokenizer[word])
+            except:
+                tokens.append(tokenizer['<unk>'])
+        all_candidates_tokens.append(tokens)
+    return all_candidates_tokens
+
+
+def get_padded_sequence(emb_sent: List[List[float]], tokens: List[int], n_candidates: int):
+    a = tf.keras.preprocessing.sequence.pad_sequences([emb_sent for _ in range(n_candidates)], maxlen=45, dtype='float32', padding='post')
+    b = tf.keras.preprocessing.sequence.pad_sequences(tokens, maxlen=45, dtype='int32', padding='post')
+    return a, b
+
+
+if __name__ == "__main__":
+    abstract = args.abstract
+    candidates = args.candidates
+
+    splited_sentences = preprocess_abstract(abstract)
+    emb_sent = embedding(splited_sentences)
+
+    candidates = preprocess_cadidates(candidates)
+    print(f'candidates: {candidates}')
+    n_candidates = len(candidates)
+    tokens = get_tokens(candidates)
+    print(f'tokens: {tokens}')
+
+    
+
+    abstract, candidates = get_padded_sequence(emb_sent=emb_sent, tokens=tokens, n_candidates=n_candidates)
+
+    y_pred_logits = model((abstract, candidates))
+    y_pred_prob = tf.nn.softmax(y_pred_logits, axis=0)
+
+    print(y_pred_prob.numpy())
